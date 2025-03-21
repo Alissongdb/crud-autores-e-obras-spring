089bd4f92dd96616930011fa2c2a533251a2ebd0
Alissongdb <alisson.gabriel5@hotmail.com>
2025-03-20 21:05:02 -0300

    criacao da controller

--- src/main/java/com/alisson/autores/e/obras/gestao/autores/Autor.java
+++ src/main/java/com/alisson/autores/e/obras/gestao/autores/Autor.java
@@ -34,4 +34,80 @@ public class Autor {
     @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
     private List<Obra> obras;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(LocalDate dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+
+    public String getPaisOrigem() {
+        return paisOrigem;
+    }
+
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrigem = paisOrigem;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Obra> getObras() {
+        return obras;
+    }
+
+    public void setObras(List<Obra> obras) {
+        this.obras = obras;
+    }
+
+    public Autor(Long id, Sexo sexo, String nome, String email, LocalDate dtNascimento, String paisOrigem, Integer cpf, List<Obra> obras) {
+        this.id = id;
+        this.sexo = sexo;
+        this.nome = nome;
+        this.email = email;
+        this.dtNascimento = dtNascimento;
+        this.paisOrigem = paisOrigem;
+        this.cpf = cpf;
+        this.obras = obras;
+    }
+
+    public Autor() {}
 }
--- src/main/java/com/alisson/autores/e/obras/gestao/obras/Obra.java
+++ src/main/java/com/alisson/autores/e/obras/gestao/obras/Obra.java
@@ -27,4 +27,54 @@ public class Obra {
     @ManyToOne
     @JoinColumn(name = "autor_id")
     private Autor autor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDtPublicacao() {
+        return dtPublicacao;
+    }
+
+    public void setDtPublicacao(LocalDate dtPublicacao) {
+        this.dtPublicacao = dtPublicacao;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Obra(Long id, String nome, String descricao, LocalDate dtPublicacao, Autor autor) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dtPublicacao = dtPublicacao;
+        this.autor = autor;
+    }
+
+    public Obra() {}
 }
